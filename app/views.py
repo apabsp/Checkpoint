ad4bb91934cb2b1f2668301a9a0412251f46abfe
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2024-04-05 00:29:44 -0300

    refactor: global getUser function

--- app/views.py
+++ app/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
 
-# Create your views here.
+def getUser(req):
+    user = User.objects.get(username=req.user)
+    context = {
+        "user": {
+            "username": user.username,
+            "email": user.email
+        }
+    }
+    return context
+
 class RootView(View):
     def get(self, req):
         if(req.user.is_authenticated):
-            context = self._getUser(req)
-            return render(req, 'app/app.html', context)
-        
-    def post(self, req):
-        if(req.user.is_authenticated):
-            context = self._getUser(req)
-            search = req.POST.get("search")
-            print(search)
+            context = getUser(req)
             return render(req, 'app/app.html', context)
         
     def _getUser(self, req):
@@ -29,18 +31,8 @@ class RootView(View):
 class SearchView(View):
     def get(self, req):
         if(req.user.is_authenticated):
-            context = self._getUser(req)
+            context = getUser(req)
             
             searchTerm = req.GET.get("search")
 
             return render(req, 'app/search.html', context)
-        
-    def _getUser(self, req):
-        user = User.objects.get(username=req.user)
-        context = {
-            "user": {
-                "username": user.username,
-                "email": user.email
-            }
-        }
-        return context
