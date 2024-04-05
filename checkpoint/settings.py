606fadf3235973e3ad9bf8001a2f1e2c518ad220
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2024-04-05 10:00:28 -0300

    remove db.sqlite3 from settings

--- checkpoint/settings.py
+++ checkpoint/settings.py
@@ -107,12 +107,12 @@ WSGI_APPLICATION = 'checkpoint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
