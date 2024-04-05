5845cd2e339f7f5e6bddbbdabbb2be801275f9da
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2024-04-05 13:14:04 -0300

    feature: game page, like game

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ app_name = 'app'
 urlpatterns = [
     path('', views.RootView.as_view(), name='root'),
     path('search/', views.SearchView.as_view(), name='search'),
+    path('game/<int:id>', views.GameView.as_view(), name='game'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
 from .models import Game
+from django.http import JsonResponse
 
 def getUser(req):
     user = User.objects.get(username=req.user)
@@ -41,3 +42,44 @@ class SearchView(View):
             context["games"] = games
 
             return render(req, 'app/search.html', context)
+
+class GameView(View):
+    def get(self, req, id):
+        if(req.user.is_authenticated):
+            context = getUser(req)
+            
+            try:
+                game = Game.objects.get(pk=id)
+                likes = game.like_set.all()
+
+                context["game"] = game
+                try:
+                    context["liked"] = likes.get(user=req.user).liked
+                except:
+                    context["liked"] = False
+
+                context["likes"] = len(likes)
+
+                return render(req, 'app/game.html', context)
+            except:
+                return redirect("app:root")
+            
+    def post(self, req, id):
+        '''like game'''
+        if(req.user.is_authenticated):
+            try:    
+                game = Game.objects.get(pk=id)
+
+                likes = game.like_set.filter(user=req.user)
+
+                if not likes.exists():
+                    game.like_set.create(user=req.user, liked=True)
+                    game.save()
+                else:
+                    for like in likes:
+                        like.liked = not like.liked
+                        like.save()
+
+                return JsonResponse({"message": "Curtiu" if like.liked else "Deixou de curtir"})
+            except:
+                return JsonResponse({"message": "Jogo nao existe"}, status=404)
